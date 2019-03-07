38036b279250cd6cb75b4689f8d46f54ad704b05
toto <thomashavy@hotmail.fr>
2019-03-07 14:45:29 +0100

    Added script to generate the room

---
+++ Bidirectional_interface/Assets/Editor/RoomGeneratorEditor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(RoomGenerator))]
+public class RoomGeneratorEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        RoomGenerator myScript = (RoomGenerator)target;
+        if (GUILayout.Button("Generate"))
+        {
+            myScript.GenerateRoom();
+        }
+    }
+}
