d1d21b2452fe955d448326a65161ba0a885b267b
toto <thomashavy@hotmail.fr>
2019-03-21 15:25:08 +0100

    Use Douglas-Peucker algorithm to simplify trajectory from file

--- Bidirectional_interface/Assets/Scripts/HandAbsolutePositionControl.cs
+++ Bidirectional_interface/Assets/Scripts/HandAbsolutePositionControl.cs
@@ -7,14 +7,30 @@ public class HandAbsolutePositionControl : MonoBehaviour
     public TextAsset data;
     public KeypointControl droneKeypointControl;
 
+    public float simplifyTrajectoryTolerance = 0.2f;
+
     private const int nbParameters = 8; // id + position + quaternion
 
     void Start()
     {
-        string text = data.text;  //this is the content as string
-        string[] splitted = text.Split(new char[] { ' ', ',', '[', ']' }, System.StringSplitOptions.RemoveEmptyEntries);
+        string[] splitted = data.text.Split(new char[] { ' ', ',', '[', ']' }, System.StringSplitOptions.RemoveEmptyEntries);
+        int nbPositions = splitted.Length / nbParameters;
 
-        int nbPositions = splitted.Length / (nbParameters);
+        Debug.Log(nbPositions);
+
+        List<Vector3> rawPositions = new List<Vector3>(nbPositions);
+        List<Vector3> filteredPositions = new List<Vector3>();
+
+        for (int i = 0; i < nbPositions; i++)
+        {
+            rawPositions.Add(new Vector3(float.Parse(splitted[nbParameters * i + 1]), float.Parse(splitted[nbParameters * i + 2]), float.Parse(splitted[nbParameters * i + 3])));
+        }
+
+        LineUtility.Simplify(rawPositions, simplifyTrajectoryTolerance, filteredPositions);
+        nbPositions = filteredPositions.Count;
+
+        Debug.Log(nbPositions);
+
         Transform[] targets = new Transform[nbPositions];
 
         GameObject targetParent = new GameObject("Targets");
@@ -22,15 +38,13 @@ public class HandAbsolutePositionControl : MonoBehaviour
         // Read positions from parsed file
         for (int i = 0; i < nbPositions; i++)
         {
-            Vector3 position = new Vector3(float.Parse(splitted[nbParameters * i + 1]), float.Parse(splitted[nbParameters * i + 2]), float.Parse(splitted[nbParameters * i + 3]));
-
             GameObject target = GameObject.CreatePrimitive(PrimitiveType.Sphere);
             target.name = "Target " + i.ToString();
             target.transform.localScale = new Vector3(0.05f, 0.05f, 0.05f);
 
             //GameObject target = new GameObject("Target " + i.ToString());
             target.transform.parent = targetParent.transform;
-            target.transform.position = position;
+            target.transform.position = filteredPositions[i];
 
             targets[i] = target.transform;
         }
