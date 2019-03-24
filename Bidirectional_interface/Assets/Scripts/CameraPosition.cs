5eb61deefb9dd37370ccc9b6a3f133cc7074c49a
Antoine <a.weber@epfl.ch>
2019-03-24 18:26:11 +0100

    Simplified Arena + FPS/TPS cameras

---
+++ Bidirectional_interface/Assets/Scripts/CameraPosition.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPosition : MonoBehaviour
+{
+    public Camera Camera;
+    public bool FPS = true;
+
+    private Vector3 cameraOffset = new Vector3(0.0f, -0.1f, 0.5f);
+
+    public void Update()
+    {
+        if (FPS)
+        {
+            // Camera position
+            Camera.transform.position = transform.position;
+
+            // Camera angles
+            Camera.transform.eulerAngles = transform.eulerAngles;
+        }
+        else
+        {
+            // Camera position
+            Camera.transform.position = transform.TransformDirection(transform.InverseTransformDirection(transform.position) - cameraOffset);
+            // Camera angles
+            Camera.transform.eulerAngles = transform.eulerAngles;
+        }
+    }
+
+}
