5b968eb1191cc5c2de14309d56afd2a1107972e0
toto <thomashavy@hotmail.fr>
2019-03-25 13:31:55 +0100

    Created base script for clutch position control (1st person view)

---
+++ Bidirectional_interface/Assets/Scripts/HandClutchPositionControl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PositionControl))]
+public class HandClutchPositionControl : MonoBehaviour
+{
+    public bool drawHand = true;
+
+    private PositionControl dronePositionControl;
+    private GameObject handTarget;
+
+    void Start()
+    {
+        dronePositionControl = GetComponent<PositionControl>();
+
+        handTarget = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        Destroy(handTarget.GetComponent<Collider>());
+        handTarget.name = "Hand Target";
+        handTarget.transform.localScale = 0.5f * SimulationData.DroneSize * Vector3.one;
+    }
+    
+    void Update()
+    {
+        handTarget.transform.position = Vector3.zero; // Get position from mocap
+
+        dronePositionControl.target = handTarget.transform;
+
+        if (drawHand)
+            handTarget.SetActive(true);
+        else
+            handTarget.SetActive(false);
+    }
+}
