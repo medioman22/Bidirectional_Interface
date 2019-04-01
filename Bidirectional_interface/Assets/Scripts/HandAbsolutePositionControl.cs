56274bb5f5fa039e093ab71eb4da3387a5e3cdc2
Thomas <thomashavy@hotmail.fr>
2019-04-01 11:51:50 +0200

    Finished absolute position control with end

--- Bidirectional_interface/Assets/Scripts/HandAbsolutePositionControl.cs
+++ Bidirectional_interface/Assets/Scripts/HandAbsolutePositionControl.cs
@@ -5,12 +5,17 @@ using UnityEngine;
 [RequireComponent(typeof(PositionControl))]
 public class HandAbsolutePositionControl : MonoBehaviour
 {
-    public VirtualHand hand;
+    public UDPCommandManager commandManager;
+    public Vector3 handReachCube = 0.7f * Vector3.one;
+    public float inputRotation = 0.0f;
+    //public VirtualHand hand;
     public bool drawHandTarget = true;
 
     private PositionControl dronePositionControl;
     private GameObject handTarget;
     private Vector3 handRoomScaling;
+
+    private Vector3 handOrigin;
 
     void Start()
     {
@@ -21,14 +26,25 @@ public class HandAbsolutePositionControl : MonoBehaviour
         handTarget.name = "Hand Target";
         handTarget.transform.localScale = 0.5f * SimulationData.DroneSize * Vector3.one;
 
-        handRoomScaling.x = SimulationData.RoomDimensions.x / hand.handReachCube.x;
-        handRoomScaling.y = SimulationData.RoomDimensions.y / hand.handReachCube.y;
-        handRoomScaling.z = SimulationData.RoomDimensions.z / hand.handReachCube.z;
+        handRoomScaling.x = SimulationData.RoomDimensions.x / handReachCube.x;
+        handRoomScaling.y = SimulationData.RoomDimensions.y / handReachCube.y;
+        handRoomScaling.z = SimulationData.RoomDimensions.z / handReachCube.z;
     }
     
     void Update()
     {
-        Vector3 targetPosition = Vector3.Scale(hand.GetHandPosition(), handRoomScaling);
+        Vector3 rawHandPosition = commandManager.GetPosition();
+        
+        // Reset referential for hand
+        if (Input.GetKey(KeyCode.R))
+        {
+            SetHandOrigin(rawHandPosition);
+        }
+
+        // Position of the hand relative to the origin point of the "box"
+        Vector3 handBoxPosition = rawHandPosition - handOrigin;
+
+        Vector3 targetPosition = ScaleHandPosition(handBoxPosition); 
         handTarget.transform.position = targetPosition; // Get position from mocap
 
         dronePositionControl.target = handTarget.transform;
@@ -37,5 +53,20 @@ public class HandAbsolutePositionControl : MonoBehaviour
             handTarget.SetActive(true);
         else
             handTarget.SetActive(false);
+    }
+
+    private void SetHandOrigin(Vector3 origin)
+    {
+        // Make sure the center of the box corresponds to the center of the room
+        origin.y -= handReachCube.y / 2;
+
+        handOrigin = origin;
+    }
+
+    private Vector3 ScaleHandPosition(Vector3 handPosition)
+    {
+        Quaternion rotation = Quaternion.Euler(0, inputRotation, 0);
+        handPosition = rotation * handPosition;
+        return Vector3.Scale(handPosition, handRoomScaling);
     }
 }
