9f55bb1ad26cece90c9f55f2d66e5f36f6c231e4
toto <thomashavy@hotmail.fr>
2019-04-04 09:31:19 +0200

    Position control with controller

--- Bidirectional_interface/Assets/Scripts/HandAbsolutePositionControl.cs
+++ Bidirectional_interface/Assets/Scripts/HandAbsolutePositionControl.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class HandAbsolutePositionControl : MonoBehaviour
 {
     public UDPCommandManager commandManager;
+    private PositionControl dronePositionControl;
+
     public Vector3 handReachCube = 0.7f * Vector3.one;
 
     [Tooltip("Rotate mocap input along y axis, for instance to align with observation angle")]
@@ -13,15 +15,19 @@ public class HandAbsolutePositionControl : MonoBehaviour
     //public VirtualHand hand;
     public bool drawHandTarget = true;
 
-    private PositionControl dronePositionControl;
+    [Tooltip("Read inputs from a controller instead of motion capture.")]
+    public bool useController = false;
+    public float controllerSpeed = 0.025f;
+
+    // For motion capture inputs
     private GameObject handTarget;
     private Vector3 handRoomScaling;
-
     private Vector3 handOrigin;
 
     void Start()
     {
         dronePositionControl = GetComponent<PositionControl>();
+        dronePositionControl.ignoreOrientation = true;
 
         handTarget = GameObject.CreatePrimitive(PrimitiveType.Cube);
         Destroy(handTarget.GetComponent<Collider>());
@@ -35,21 +41,39 @@ public class HandAbsolutePositionControl : MonoBehaviour
     
     void Update()
     {
-        Vector3 rawHandPosition = commandManager.GetPosition();
-        
-        // Reset referential for hand
-        if (Input.GetKey(KeyCode.Mouse1))
+        if (useController)
         {
-            SetHandOrigin(rawHandPosition);
+            Debug.Log("H:" + Input.GetAxis("Horizontal"));
+            Debug.Log("V:" + Input.GetAxis("Vertical"));
+            Debug.Log("A:" + Input.GetAxis("Altitude"));
+            Debug.Log("R:" + Input.GetAxis("Rotation"));
+            Debug.Log("C:" + Input.GetAxis("Clutch"));
+
+            float h = Input.GetAxis("Horizontal");
+            float v = Input.GetAxis("Vertical");
+            float a = Input.GetAxis("Altitude");
+
+            handTarget.transform.position += Quaternion.Euler(0, inputRotation, 0) * new Vector3(h, a, v) * controllerSpeed;
+            dronePositionControl.target = handTarget.transform;
         }
+        else // Mocap inputs
+        {
+            Vector3 rawHandPosition = commandManager.GetPosition();
 
-        // Position of the hand relative to the origin point of the "box"
-        Vector3 handBoxPosition = rawHandPosition - handOrigin;
+            // Reset referential for hand
+            if (Input.GetKey(KeyCode.Mouse1))
+            {
+                SetHandOrigin(rawHandPosition);
+            }
 
-        Vector3 targetPosition = ScaleHandPosition(handBoxPosition); 
-        handTarget.transform.position = targetPosition; // Get position from mocap
+            // Position of the hand relative to the origin point of the "box"
+            Vector3 handBoxPosition = rawHandPosition - handOrigin;
 
-        dronePositionControl.target = handTarget.transform;
+            Vector3 targetPosition = ScaleHandPosition(handBoxPosition);
+            handTarget.transform.position = targetPosition; // Get position from mocap
+
+            dronePositionControl.target = handTarget.transform;
+        }
 
         if (drawHandTarget)
             handTarget.SetActive(true);
