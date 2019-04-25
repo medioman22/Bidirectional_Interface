28b480672c3d17efba6f9eb273a475fa4208ff90
toto <thomashavy@hotmail.fr>
2019-04-25 15:51:02 +0200

    Started implementing cooperative control

---
+++ Bidirectional_interface/Assets/Scripts/CooperativeControl.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CooperativeControl : MonoBehaviour
+{
+    private List<MagnetConnector> connectors;
+
+    private GameObject handTarget;
+    private GameObject[] droneTargets;
+
+    public bool AllConnected
+    {
+        get 
+        {
+            bool detached = false;
+
+            foreach (var connector in connectors)
+            {
+                // Check if any connector has no drone attached
+                if (connector.ConnectedDroneController == null)
+                    detached = true;
+            }
+
+            return !detached;
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(1.0f, 0.5f, 0.0f);
+        
+        for (int i = 0; i < connectors.Count; i++)
+        {
+            // Instantiate target
+            Gizmos.DrawCube(droneTargets[i].transform.position, Vector3.one * 0.025f);
+        }
+    }
+    
+    void Start()
+    {
+        connectors = new List<MagnetConnector>(GetComponents<MagnetConnector>());
+
+        // Instantiate hand target
+        handTarget = new GameObject("Hand Target");
+        handTarget.transform.position = transform.position;
+        handTarget.transform.rotation = transform.rotation;
+        handTarget.transform.localScale = transform.localScale;
+
+        droneTargets = new GameObject[connectors.Count];
+        for (int i = 0; i < connectors.Count; i++)
+        {
+            // Instantiate target
+            droneTargets[i] = new GameObject("Drone Target " + i.ToString());
+            droneTargets[i].transform.position = connectors[i].AnchorPosition + Vector3.up * connectors[i].linkDistance;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float targetYaw = 0.0f;
+        float targetInclination = 0.0f;
+
+        // Apply cooperative control
+        if (AllConnected)
+        {
+            // Set targets position
+            for (int i = 0; i < connectors.Count; i++)
+            {
+                droneTargets[i].transform.position = handTarget.transform.TransformPoint(connectors[i].anchor) + Vector3.up * connectors[i].linkDistance;
+
+                if (connectors[i].ConnectedDroneController != null)
+                {
+                    connectors[i].ConnectedDroneController.target = droneTargets[i].transform;
+                }
+            }
+        }
+        else
+        {
+            // Set targets position
+            for (int i = 0; i < connectors.Count; i++)
+            {
+                droneTargets[i].transform.position = handTarget.transform.TransformPoint(connectors[i].anchor) + Vector3.up * connectors[i].linkDistance;
+
+                if (connectors[i].ConnectedDroneController != null)
+                {
+                    connectors[i].ConnectedDroneController.target = droneTargets[i].transform;
+                }
+            }
+        }
+    }
+}
--- Bidirectional_interface/Assets/Scripts/MagnetConnector.cs
+++ Bidirectional_interface/Assets/Scripts/MagnetConnector.cs
@@ -2,25 +2,112 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(CooperativeControl))]
 public class MagnetConnector : MonoBehaviour
 {
     public float linkDistance = 0.2f;
     public Vector3 anchor = Vector3.zero;
 
+    private Rigidbody rgbd;
+    private CooperativeControl controller;
+
     private SphereCollider magnetCollider;
+    private SpringJoint joint;
+    private bool connected = false;
+    private HandClutchPositionControl connectedMocapController;
+    private PositionControl _connectedDroneController;
+
+    public PositionControl ConnectedDroneController
+    {
+        get 
+        {
+            return _connectedDroneController;
+        }
+    }
+
+    public Vector3 AnchorPosition
+    {
+        get
+        {
+            return transform.TransformPoint(anchor);
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
     {
+        rgbd = GetComponent<Rigidbody>();
+        controller = GetComponent<CooperativeControl>();
+
         magnetCollider = gameObject.AddComponent(typeof(SphereCollider)) as SphereCollider;
         magnetCollider.center = anchor;
         magnetCollider.radius = linkDistance;
         magnetCollider.isTrigger = true;
     }
 
+    void Update()
+    {
+        // Check if joint is broken
+        if (joint == null)
+        {
+            connected = false;
+
+            // Enable mocap control if previously attached to a drone
+            if (connectedMocapController != null)
+            {
+                connectedMocapController.enabled = true;
+                connectedMocapController = null;
+                _connectedDroneController = null;
+            }
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawCube(transform.TransformPoint(anchor), Vector3.one * 0.025f);
+    }
+
     void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Drone")
-            Debug.Log("Drone");
+        {
+            if (!connected)
+            {
+                if (CreateLink(other.gameObject))
+                    connected = true;
+                else
+                    connected = false;
+            }
+        }
+    }
+
+    // Creates a spring joint between this object and the colliding drone, if the drone has no joint yet
+    // Return true if successful
+    bool CreateLink(GameObject drone)
+    {
+        // Check if drone already connected
+        if (drone.GetComponent<SpringJoint>() != null)
+        {
+            return false;
+        }
+
+        // Created link
+        joint = drone.AddComponent(typeof(SpringJoint)) as SpringJoint;
+        joint.connectedBody = rgbd;
+        joint.connectedAnchor = anchor;
+        joint.spring = 100f;
+        joint.damper = 100f;
+        joint.maxDistance = 0.2f;
+        joint.tolerance = 0.01f;
+        joint.breakForce = 2f;
+
+        // Disable mocap control
+        connectedMocapController = drone.GetComponent<HandClutchPositionControl>();
+        connectedMocapController.enabled = false;
+        _connectedDroneController = drone.GetComponent<PositionControl>();
+
+        return true;
     }
 }
