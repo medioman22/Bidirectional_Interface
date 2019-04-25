827f87b9e9bd57cb5024f31e401de70e9a1cc2bb
toto <thomashavy@hotmail.fr>
2019-04-25 12:35:27 +0200

    Began to implement magnet attachment script

---
+++ Bidirectional_interface/Assets/Scripts/MagnetConnector.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagnetConnector : MonoBehaviour
+{
+    public float linkDistance = 0.2f;
+    public Vector3 anchor = Vector3.zero;
+
+    private SphereCollider magnetCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        magnetCollider = gameObject.AddComponent(typeof(SphereCollider)) as SphereCollider;
+        magnetCollider.center = anchor;
+        magnetCollider.radius = linkDistance;
+        magnetCollider.isTrigger = true;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Drone")
+            Debug.Log("Drone");
+    }
+}
