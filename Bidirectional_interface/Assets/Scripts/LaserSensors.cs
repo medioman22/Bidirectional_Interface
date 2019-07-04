05f9335b1038f4b01103b6ca97ec32b43c8a9242
Antoine <a.weber@epfl.ch>
2019-07-04 16:06:49 +0200

    Observator frame for feedback when in TPV

--- Bidirectional_interface/Assets/Scripts/LaserSensors.cs
+++ Bidirectional_interface/Assets/Scripts/LaserSensors.cs
@@ -4,6 +4,17 @@ using UnityEngine;
 
 public class LaserSensors : MonoBehaviour
 {
+    private DroneCamera cameraPosition;
+
+    public HandClutchPositionControl posControl;
+    public float theta;
+
+
+    float bufferRight;
+    float bufferLeft;
+    float bufferFront;
+    float bufferBack;
+
     [System.Serializable]
     public class ObstacleDistances
     {
@@ -19,6 +30,7 @@ public class LaserSensors : MonoBehaviour
 
     void Start()
     {
+        cameraPosition = GetComponent<DroneCamera>();
         allDistances = new ObstacleDistances();
     }
 
@@ -56,6 +68,64 @@ public class LaserSensors : MonoBehaviour
         }
         if (Physics.Raycast(transform.position, transform.right, out rightHit)) {
             allDistances.rightObstacle = rightHit.distance;
+        }
+
+        // if in TPV, observator frame for feedback.
+        if (!cameraPosition.FPS)
+        {
+            // angle between the observer angle (which is rotated by observationinputrotation with respect to unity frame) and the drone angle.
+            theta = Mathf.DeltaAngle(posControl.observationInputRotation, transform.eulerAngles.y);
+
+            // project the measured distance to represent the true distance with respect to the angle of observation
+            if(theta > -45 && theta < 45)
+            {
+                allDistances.frontObstacle *= Mathf.Abs(Mathf.Cos(theta * Mathf.PI / 180));
+                allDistances.rightObstacle *= Mathf.Abs(Mathf.Cos(theta * Mathf.PI / 180));
+                allDistances.leftObstacle *= Mathf.Abs(Mathf.Cos(theta * Mathf.PI / 180));
+                allDistances.backObstacle *= Mathf.Abs(Mathf.Cos(theta * Mathf.PI / 180));
+            }
+            else if (theta > -135 && theta < -45)
+            {
+                // to have right angles for projection
+                theta += 90;
+
+                bufferFront = Mathf.Abs(Mathf.Cos(theta * Mathf.PI / 180)) * allDistances.rightObstacle;
+                bufferRight = Mathf.Abs(Mathf.Cos(theta * Mathf.PI / 180)) * allDistances.backObstacle;
+                bufferLeft = Mathf.Abs(Mathf.Cos(theta * Mathf.PI / 180)) * allDistances.frontObstacle;
+                bufferBack = Mathf.Abs(Mathf.Cos(theta * Mathf.PI / 180)) * allDistances.leftObstacle;
+
+                allDistances.frontObstacle = bufferFront;
+                allDistances.backObstacle = bufferBack;
+                allDistances.leftObstacle = bufferLeft;
+                allDistances.rightObstacle = bufferRight;
+            }
+            else if (theta > 45 && theta < 135)
+            {
+                // to have right angles for projection
+                theta -= 90;
+
+                bufferFront = Mathf.Abs(Mathf.Cos(theta * Mathf.PI / 180)) * allDistances.leftObstacle;
+                bufferRight = Mathf.Abs(Mathf.Cos(theta * Mathf.PI / 180)) * allDistances.frontObstacle;
+                bufferLeft = Mathf.Abs(Mathf.Cos(theta * Mathf.PI / 180)) * allDistances.backObstacle;
+                bufferBack = Mathf.Abs(Mathf.Cos(theta * Mathf.PI / 180)) * allDistances.rightObstacle;
+
+                allDistances.frontObstacle = bufferFront;
+                allDistances.backObstacle = bufferBack;
+                allDistances.leftObstacle = bufferLeft;
+                allDistances.rightObstacle = bufferRight;
+            }
+            else if (Mathf.Abs(theta) > 135)
+            {
+                bufferFront = Mathf.Abs(Mathf.Cos(theta * Mathf.PI / 180)) * allDistances.backObstacle;
+                bufferRight = Mathf.Abs(Mathf.Cos(theta * Mathf.PI / 180)) * allDistances.leftObstacle;
+                bufferLeft = Mathf.Abs(Mathf.Cos(theta * Mathf.PI / 180)) * allDistances.rightObstacle;
+                bufferBack = Mathf.Abs(Mathf.Cos(theta * Mathf.PI / 180)) * allDistances.frontObstacle;
+
+                allDistances.frontObstacle = bufferFront;
+                allDistances.backObstacle = bufferBack;
+                allDistances.leftObstacle = bufferLeft;
+                allDistances.rightObstacle = bufferRight;
+            }
         }
     }
 }
