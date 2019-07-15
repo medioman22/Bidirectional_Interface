1f5bb9d937255a16c8ec10da1d8365cddc83203b
Thomas <thomashavy@hotmail.fr>
2019-07-15 13:23:27 +0200

    Added script to start/stop logger at convenient locations

---
+++ Bidirectional_interface/Assets/Scripts/LogCheckPoint.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+[RequireComponent(typeof(Rigidbody))]
+public class LogCheckPoint : MonoBehaviour
+{
+    public DataLogger logger;
+
+    [Tooltip("If true, the logger will be activated when the drone passes through the obstacle. If false, it will be deactivated")]
+    public bool recordOnCollision = true;
+    public bool hideObjectInPlayMode = true;
+
+    void Start()
+    {
+        if (hideObjectInPlayMode)
+        {
+            Renderer renderer = GetComponent<Renderer>();
+
+            if (renderer != null)
+            {
+                renderer.enabled = false;
+            }
+        }
+    }
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Drone")
+            logger.recording = recordOnCollision;
+    }
+}
